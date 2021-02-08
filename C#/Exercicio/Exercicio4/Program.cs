f2f6654d90fc4f2e629b54a51e46cec14901d1c5
Leonardo guedes <leonardo.guedes.rocha@gmail.com>
2021-02-08 15:09:11 -0300

    Exercicio4Correcao

--- C#/Exercicio/Exercicio4/Program.cs
+++ C#/Exercicio/Exercicio4/Program.cs
@@ -6,63 +6,88 @@ namespace Exercicio4
     {
         static void Main(string[] args)
         {
+
+            void fatorial(int numero, int fat)
+            {
+                for (var i = 1; i < numero; i++)
+                {
+                    fat += fat * i;
+                }
+                Console.WriteLine($"O Valor do fatorial que você digitou e:{fat}");
+            }; 
+
+            void VerificarPrimo(int numero, int cont)
+            {
+                for (var i = 1; i < numero; i++)
+                {
+                    if (numero % i == 0)
+                        cont++;
+                }
+                if (cont == 2)
+                    Console.WriteLine($"O numero {numero} e primo!");
+                else
+                    Console.WriteLine($"O numero {numero} nao primo!");
+            };
+
+            void VerificarParImpar(int numero)
+            {
+                if (numero % 2 == 0)
+                    Console.WriteLine($"O numero que voce digitou e par {numero}.");
+                else
+                    Console.WriteLine($"O numero que voce digitou e impar {numero}.");
+            };
+
+            void cubo(int numero)
+            {
+                if (numero > 0)
+                {
+                    double cubo;
+                    cubo = Math.Pow(numero, 3);
+                    Console.WriteLine($"O valor do numero elevado ao cubo {cubo}.");
+                }
+            };
+
             int numero;
-            int fatorial = 1;
+            int fat = 1;
             int cont = 1;
-            Console.WriteLine("Digite um numero: ");
-            numero = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
+            string resp = "S";
 
-            Console.WriteLine("--------------------------------------------------------------------------------------------------");
-            Console.WriteLine("| A seguir escolha qual figura deseja, digitando o número correspondente a figura correspondente.|");
-            Console.WriteLine("|         1 ==> Calcular o Fatorial para o numero lido                                           |");
-            Console.WriteLine("|         2 ==> Verificar se o numero é Primo                                                    |");
-            Console.WriteLine("|         3 ==> Verificar se é Par ou Impar                                                      |");
-            Console.WriteLine("|         4 ==>  Calcular o número ao cubo                                                       |");
-            Console.WriteLine("--------------------------------------------------------------------------------------------------");
-            Console.WriteLine("Qual dessa opções voce deseja executar?");
-            string opcao = Console.ReadLine();
+            while(resp == "S" || resp == "s"){
+                Console.WriteLine("Digite um numero: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
 
-            switch (opcao)
+                Console.WriteLine("--------------------------------------------------------------------------------------------------");
+                Console.WriteLine("| A seguir escolha qual figura deseja, digitando o número correspondente a figura correspondente.|");
+                Console.WriteLine("|         1 ==> Calcular o Fatorial para o numero lido                                           |");
+                Console.WriteLine("|         2 ==> Verificar se o numero é Primo                                                    |");
+                Console.WriteLine("|         3 ==> Verificar se é Par ou Impar                                                      |");
+                Console.WriteLine("|         4 ==>  Calcular o número ao cubo                                                       |");
+                Console.WriteLine("--------------------------------------------------------------------------------------------------");
+                Console.WriteLine("Qual dessa opções voce deseja executar?");
+                string opcao = Console.ReadLine();
+
+                switch (opcao)
             {
                 case "1":
-                    for (var i = 1; i < numero; i++)
-                    {
-                        fatorial += fatorial * i;
-                    }
-                    Console.WriteLine($"O Valor do fatorial que você digitou e:{fatorial}");
+                        fatorial(numero, fat);
                     break;
                 case "2":
-                    for (var i = 1; i < numero; i++)
-                    {
-                        if (numero % i == 0)
-                            cont++;
-                    }
-                    if (cont == 2)
-                        Console.WriteLine($"O numero {numero} e primo!");
-                    else
-                        Console.WriteLine($"O numero {numero} nao primo!");
+                        VerificarPrimo(numero, cont);
                     break;
                 case "3":
-                    if (numero % 2 == 0)
-                        Console.WriteLine($"O numero que voce digitou e par {numero}.");
-                    else
-                        Console.WriteLine($"O numero que voce digitou e impar {numero}.");
+                        VerificarParImpar(numero);
                     break;
                 case "4":
-                    if (numero > 0)
-                    {
-                        double cubo;
-                        cubo = Math.Pow(numero, 3);
-                        Console.WriteLine($"O valor do numero elevado ao cubo {cubo}.");
-                    }
+                        cubo(numero);
                     break;
                 default:
                     Console.WriteLine("Opcao Invalida!");
                     break;
+            }  
+                    Console.WriteLine("Deseja continuar?- Resposta [S]im ou [N]ao:");
+                    resp = Convert.ToString(Console.ReadLine());
             }
-                    
-
         }
     }
 }
--- Node.js/Exercicio4.js
+++ Node.js/Exercicio4.js
@@ -13,8 +13,45 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-let fatorial=1;
+function fatorial(numero,fat){
+    for (var i = 1; i < numero; i++)
+       {
+        fat += fat * i;
+       }
+       console.log(`O Valor do fatorial que você digitou e:${fat}.`);
+}
+function VerficarPrimo(numero,cont){
+    for (var i = 1; i < numero; i++)
+    {
+        if (numero % i == 0)
+           cont++;
+    }
+    if(cont == 2)
+        console.log(`O numero  ${numero} e primo!`);
+    else
+        console.log(`O numero  ${numero} nao e primo!`);
+}
+function ParOuImpar(numero){
+    if(numero % 2 == 0)
+    {
+        console.log(`O numero que voce digitou e par${numero}`);
+    }else{
+        console.log(`O numero que voce digitou e impar ${numero}.`);
+    }
+}
+function cubo(numero){
+    if(numero > 0)
+    {
+        numero = Math.pow(numero,3);
+        console.log(`O valor do numero elevado ao cubo ${numero}.`);
+    }
+}
+
+let fat=1;
 let cont = 1;
+let resp = "S";
+
+while((resp = "S") || (resp = "s")){    
 rl.question("Digite um numero: ", function(numero) {
     console.log ("--------------------------------------------------------------------------------------------------");
     console.log("| A seguir escolha qual figura deseja, digitando o número correspondente a figura correspondente.|");
@@ -26,40 +63,20 @@ rl.question("Digite um numero: ", function(numero) {
     rl.question("Qual dessa opções voce deseja executar? ", function(opcao) {
         switch (opcao){
             case '1':
-                   for (var i = 1; i < numero; i++)
-                   {
-                       fatorial += fatorial * i;
-                   }
-                   console.log(`O Valor do fatorial que você digitou e:${fatorial}`);
+                   fatorial(numero,fat);
                    break;
              case '2':
-                    for (var i = 1; i < numero; i++)
-                    {
-                        if (numero % i == 0)
-                           cont++;
-                    }
-                    if(cont == 2)
-                        console.log(`O numero  ${numero} e primo!`);
-                    else
-                        console.log(`O numero  ${numero} nao e primo!`);
+                    VerficarPrimo(numero,cont);
                    break;
             case '3':
-                    if(numero % 2 == 0)
-                    {
-                        console.log(`O numero que voce digitou e par${numero}`);
-                    }else{
-                        console.log(`O numero que voce digitou e impar ${numero}.`);
-                    }
-                    break;
+                ParOuImpar(numero);
+                    break;  
             case '4':
-                        if(numero > 0)
-                        {
-                            numero = Math.pow(numero,3);
-                            console.log(`O valor do numero elevado ao cubo ${numero}.`)
-                        }
-                        break;
+                    cubo(numero);
+                    break;
             default:
                     console.log(`Opcao Invalida!`);
         }
     });
 });
+}
